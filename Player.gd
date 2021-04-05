9e5c230b045a0525366429293d84b6297c0b2076
GamersCrib <play.gamerscrib@gmail.com>
2021-04-05 12:26:09 +0800

    Part 6 Animation
    -Using animationTree
    -Using animationState
    -Using animationPlayer

--- Player.gd
+++ Player.gd
@@ -5,6 +5,8 @@ const MAX_SPEED = 80
 const FRINCTION = 500 
 
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 var velocity = Vector2.ZERO
 
@@ -15,8 +17,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRINCTION * delta)
- 
+
 	velocity = move_and_slide(velocity)
